4cfc6112c465d010c73d1e38c85ebeb608e2bc45
John Attai Yinusa <donattai1032@gmail.com>
2024-05-17 08:16:08 +0100

    edit config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,12 +4,12 @@ import { TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
 export const config: TypeOrmModuleAsyncOptions = {
   useFactory: async (configService: ConfigService) => ({
     type: 'mysql',
-    // url: configService.get('DB_URI'),
-    host: configService.get('DB_HOST'),
-    port: configService.get('DB_PORT'),
-    username: configService.get('DB_USERNAME'),
-    password: configService.get('DB_PASSWORD'),
-    database: configService.get('DB_DATABASE'),
+    url: configService.get('DB_URI'),
+    // host: configService.get('DB_HOST'),
+    // port: configService.get('DB_PORT'),
+    // username: configService.get('DB_USERNAME'),
+    // password: configService.get('DB_PASSWORD'),
+    // database: configService.get('DB_DATABASE'),
     synchronize: configService.get('NODE_ENV') !== 'production',
     autoLoadEntities: true,
   }),
