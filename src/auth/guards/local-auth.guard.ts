b5378c2deefd176a9e836d4e15a6a943d2b6df4c
John Attai Yinusa <donattai1032@gmail.com>
2024-08-19 11:51:59 +0100

    add handleRequest method to Local AuthGaurd

--- src/auth/guards/local-auth.guard.ts
+++ src/auth/guards/local-auth.guard.ts
@@ -1,5 +1,16 @@
-import { Injectable } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import {
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from "@nestjs/common";
+import { AuthGuard } from "@nestjs/passport";
 
 @Injectable()
-export class LocalAuthGuard extends AuthGuard('local') {}
+export class LocalAuthGuard extends AuthGuard("local") {
+  handleRequest(err: any, user: any, info: any, context: any) {
+    if (err || !user) {
+      throw new UnauthorizedException("Invalid Credentials");
+    }
+    return user;
+  }
+}
